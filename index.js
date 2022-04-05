8d4eec783c5ad1302739eca2338fc887e3d85c30
Richard Aiello <richaiello17@gmail.com>
2022-04-05 17:19:42 -0400

    Installed leaflet.js and took first steps towards implementing a map.

---
+++ index.js
@@ -0,0 +1,12 @@
+// Implements a map from leaflet.js
+
+const map = L.map('map').setView([51.505, -0.09], 13);
+
+L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}', {
+    attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
+    maxZoom: 18,
+    id: 'mapbox/streets-v11',  
+    tileSize: 512,
+    zoomOffset: -1,
+    accessToken: 'pk.eyJ1IjoicmljaGFpZWxsbzE3IiwiYSI6ImNsMG1pb2tiMjBxODczam8waWZ1ZGZ0dGEifQ.p3TioGR-2e5YJje17qFpdw'
+}).addTo(map);
